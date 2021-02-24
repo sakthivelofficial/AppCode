e3a9636a80a0863c0e18f92a8d4431d8f37efad5
sakthivelofficial <sakthivelofficial@outlook.com>
2021-02-24 22:32:17 +0530

    commit after Adding Client side Code for Academy Management System

---
+++ academyManagementSystem/src/app/login-page/login-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.scss']
+})
+export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor( 
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+  }
+
+  gotoRegister()
+  {
+    this.router.navigate(['/Register']);
+  }
+}
