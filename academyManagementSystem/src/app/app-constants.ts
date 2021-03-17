1e9590572b1abf4cc3b73aecf194811f1c303540
Puja Shree <spujas3@gmail.com>
2021-03-17 19:40:34 +0530

    Dashboard Page

--- academyManagementSystem/src/app/app-constants.ts
+++ academyManagementSystem/src/app/app-constants.ts
@@ -1,19 +1,83 @@
 export const homesidenav = [
-    {name: 'Dashboard', route:'Dashboard',class:'home-icon'},
-    {name: 'Courses',children:[
-        {name: 'New Course', route:'Courses/AddCourse'},
-        {name: 'Manage Course', route:'Courses/ManageCourse'}
-    ],isopen : false,class:'courses-icon'},
-    {name: 'Students',children:[
-        {name: 'New Student', route:'Students/AddStudent'},
-        {name: 'Manage Students', route:'Students/ManageStudents'}
-    ],isopen : false,class:'students-icon'},
-    {name: 'Staff',children:[
-        {name: 'New Staff', route:'Staff/AddStaff'},
-        {name: 'Manage Staffs', route:'Staff/ManageStaff'}
-    ],isopen : false,class:'staff-icon'},
-    {name: 'Fees',children:[
-        {name: 'Generate Fee', route:'Fee/AddFee'},
-        {name: 'Manage Fees', route:'Fee/ManageFees'}
-    ],isopen : false,class:'fee-icon'},
+  { name: "Dashboard", route: "Dashboard", class: "home-icon" },
+  {
+    name: "Courses",
+    children: [
+      { name: "New Course", route: "Courses/AddCourse" },
+      { name: "Manage Course", route: "Courses/ManageCourse" },
+    ],
+    isopen: false,
+    class: "courses-icon",
+  },
+  {
+    name: "Students",
+    children: [
+      { name: "New Student", route: "Students/AddStudent" },
+      { name: "Manage Students", route: "Students/ManageStudents" },
+    ],
+    isopen: false,
+    class: "students-icon",
+  },
+  {
+    name: "Staff",
+    children: [
+      { name: "New Staff", route: "Staff/AddStaff" },
+      { name: "Manage Staffs", route: "Staff/ManageStaff" },
+    ],
+    isopen: false,
+    class: "staff-icon",
+  },
+  {
+    name: "Fees",
+    children: [
+      { name: "Generate Fee", route: "Fee/AddFee" },
+      { name: "Manage Fees", route: "Fee/ManageFees" },
+    ],
+    isopen: false,
+    class: "fee-icon",
+  },
+];
+
+export const studentsPie = [
+  {
+    name: "guitar",
+    value: 20,
+  },
+  {
+    name: "Piano",
+    value: 30,
+  },
+  {
+    name: "Carnatic Singing",
+    value: 40,
+  },
+  {
+    name: "Drums",
+    value: 20,
+  },
+];
+
+export const teachersPie = [
+  {
+    name: "guitar",
+    value: 2,
+  },
+  {
+    name: "Piano",
+    value: 3,
+  },
+  {
+    name: "Carnatic Singing",
+    value: 2,
+  },
+  {
+    name: "Drums",
+    value: 1,
+  },
+];
+
+export const dashboradcards = [
+  { title: "Student", count: 0, isPieChar: true, PieValue: studentsPie },
+  { title: "Staffs", count: 0, isPieChar: true, PieValue: teachersPie },
+  { title: "Courses", count: 4, isPieChar: false },
 ];
--- academyManagementSystem/src/app/dashboard/dashboard.component.ts
+++ academyManagementSystem/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { dashboradcards } from "../app-constants";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
+  dashboradcards = dashboradcards;
 
-  constructor() { }
+  // Pie Chart Variables
+  view: any[] = [300, 250];
+  gradient: boolean = true;
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  isDoughnut: boolean = true;
+ // legendPosition: string = "below";
+
+  colorScheme = {
+    domain: ["#5AA454", "#A10A28", "#C7B42C", "#AAAAAA"],
+  };
+  // Pie Chart ends
+
+  constructor() {}
 
   ngOnInit() {
+    this.dashboradcards.forEach((c) => {
+      if (c.isPieChar) {
+        c.PieValue.forEach((v) => (c.count += v.value));
+      }
+    });
   }
-
 }
