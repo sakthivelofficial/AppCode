6948edf27b0b0a364a96a81c4342954379b0cc72
Puja Shree <spujas3@gmail.com>
2021-03-21 19:57:43 +0530

    Manager File

---
+++ Api/AcademyManagementSystem/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AcademyManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AcademyManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [Route("StudentAdmission")]
+        [HttpPost]
+        [SwaggerResponse(typeof(int))]
+        public async Task<int> InsertStudentAdmission(InsertStudent student)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Api/AcademyManagementSystem/Models/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademyManagementSystem.Models
+{
+    public class Student
+    {
+        public int? StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ParentName { get; set; }
+        public double ContactNumber { get; set; }
+        public string EmailId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public bool? AbroadStudent { get; set; }
+    }
+
+    public class StudentCourse {
+        public int? MappingId { get; set; }
+        public int? StudentID { get; set; }
+        public int? CourseId { get; set; }
+        public int? LevelId { get; set; }
+        public int? GradeId { get; set; }
+        public bool? isCompleted { get; set; } 
+    }
+
+    public class InsertStudent {
+        public Student student { get; set; }
+        public List<StudentCourse> courses {get; set;}
+    }
+}
