aa1856a4f1a2293b2abc2d97f97c71505073b1dc
sakthivelofficial <79590003+sakthivelofficial@users.noreply.github.com>
2021-03-22 16:32:38 +0530

    Commit after integrating add student

--- academyManagementSystem/src/app/Students/add-student/add-student.component.ts
+++ academyManagementSystem/src/app/Students/add-student/add-student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Student, StudentsClient,StudentCourse, InsertStudent } from 'src/api-client/api';
 
 @Component({
   selector: 'app-add-student',
@@ -9,7 +10,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AddStudentComponent implements OnInit {
   AdmissionForm: FormGroup;
   CourseForm: FormGroup;
-  constructor( private formBuilder: FormBuilder) { 
+  studentinfo : Student;
+  insertstudent :any = {};
+  studentcourseinfo : StudentCourse;
+  constructor( private formBuilder: FormBuilder, private studentsclient:StudentsClient) { 
     this.AdmissionForm = this.formBuilder.group({
       FirstName: ['', Validators.required],
       LastName: ['', Validators.required],
@@ -20,7 +24,7 @@ export class AddStudentComponent implements OnInit {
       City: ['', Validators.required],
       State: ['', Validators.required],
       Country: ['', Validators.required],
-      IsAbroadStudent: ['', Validators.required]
+      //IsAbroadStudent: ['', Validators.required]
 });
 this.CourseForm = this.formBuilder.group({
   CoueseName: ['', Validators.required],
@@ -31,5 +35,22 @@ this.CourseForm = this.formBuilder.group({
 
   ngOnInit() {
   }
-
+  submitadmissiondetails()
+  {
+    this.insertstudent.firstName = this.AdmissionForm.get('FirstName').value;
+    this.insertstudent.lastName = this.AdmissionForm.get('LastName').value;
+    this.insertstudent.parentName = this.AdmissionForm.get('ParentName').value;
+    this.insertstudent.contactNumber = this.AdmissionForm.get('ContactNumber').value;
+    this.insertstudent.emailId = this.AdmissionForm.get('EmailId').value;
+    this.insertstudent.address = this.AdmissionForm.get('Address').value;
+    this.insertstudent.city = this.AdmissionForm.get('City').value;
+    this.insertstudent.state = this.AdmissionForm.get('State').value;
+    this.insertstudent.state = this.AdmissionForm.get('Country').value;
+    this.insertstudent.mappingId = 1;
+    this.insertstudent.studentID = 2;
+    this.insertstudent.courseId = 3;
+    this.insertstudent.levelId = 2;
+    this.insertstudent.isCompleted = false;
+    this.studentsclient.insertStudentAdmission(this.insertstudent).subscribe();
+  }
 }
